2113ee32d1727e63da0cf7b81a1831375410fda1
Thalles Borges <thallesborg3s@gmail.com>
2024-07-22 17:28:00 -0300

    numeros

--- fundamentos/tipagem.js
+++ fundamentos/tipagem.js
@@ -1,2 +1,12 @@
 let qualquer = 'Legal'
 console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.2154
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = true
+console.log(qualquer)
+console.log(typeof qualquer)
+
