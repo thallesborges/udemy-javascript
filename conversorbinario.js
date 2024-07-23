dfeb10e91709fba74546eefd49f79b95118fb417
Thalles Borges <thallesborg3s@gmail.com>
2024-07-23 16:20:49 -0300

    23/07

---
+++ conversorbinario.js
@@ -0,0 +1,1 @@
+// Conversor binário 
---
+++ fundamentos/array.js
@@ -0,0 +1,14 @@
+const valores = [7.7, 8.9, 6.3, 9.2]
+console.log(valores[0], valores[3])
+console.log(valores[4])
+
+valores[4] = 10
+console.log(valores)
+console.log(valores.length)
+
+valores.push()
+valores.pop()
+console.log(valores)
+delete valores[0]
+valores[0] = 50
+console.log(valores)
